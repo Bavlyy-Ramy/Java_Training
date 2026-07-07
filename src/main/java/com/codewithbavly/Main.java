6067df40d5394b6732d7b174096294585b3d16a6
Bavly <bavly@gmail.com>
2026-07-07 14:43:35 +0300

    Practice 1

--- src/main/java/com/codewithbavly/Main.java
+++ src/main/java/com/codewithbavly/Main.java
@@ -3,19 +3,42 @@ package com.codewithbavly;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
+
+class Person{
+    public void eat(){
+        System.out.println("Person is eating");
+    }
+}
+
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<Integer>();
-        nums.add(6);
-        nums.add(4);
-        nums.add(5);
-        nums.add(8);
-        nums.add(9);
 
-        System.out.println(nums.get(2));
+        int x = 5;
+        System.out.println(x);
+        System.out.println(++x);
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if(x > num) System.out.println("x is greater than " + num);
 
-        System.out.println(nums);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+
+        Person p1 = new Person();
+        p1.eat();
+
+        Person persons[] = new Person[3];
+        persons[0] = new Person();
+        persons[1] = new Person();
+        persons[2] = new Person();
+
+        persons[1].eat();
+
+
+
+
 
     }
 }
